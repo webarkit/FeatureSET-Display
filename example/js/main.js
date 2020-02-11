65f56853aea50b8fbf768c7b3caca982606285fb
Walter Perdan <github@kalwaltart.it>
2020-02-12 00:33:06 +0100

    simplifying things, removing code that won't be init

--- example/js/main.js
+++ example/js/main.js
@@ -1,9 +1,6 @@
 var interval = setTimeout(function() {
   var arfset = new ARfset(1117, 893);
   console.log(arfset);
-  /*arfset.getImageSetWidth('./data/pinball.iset', function(width){
-    console.log(width);
-  })*/
   arfset.loadNFTMarker('data/pinball');
   arfset.display();
 }, 200);
