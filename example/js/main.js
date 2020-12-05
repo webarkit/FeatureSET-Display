f29f778f2864b9ab9ff315f6cae8fb5110900238
walter perdan <info@kalwaltart.it>
2020-12-05 11:14:51 +0100

    using min version, the Listener not always works

--- example/js/main.js
+++ example/js/main.js
@@ -4,3 +4,10 @@ var interval = setTimeout(function() {
   arfset.loadNFTMarker('data/pinball');
   arfset.display();
 }, 200);
+
+/*window.addEventListener('FeatureSETDisplay-loaded', function () {
+  var arfset = new ARfset(893, 1117);
+  console.log(arfset);
+  arfset.loadNFTMarker('data/pinball');
+  arfset.display();
+});*/
