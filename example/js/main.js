da079d3f1ec6bd83348354228f25d29e0f6355ab
kalwalt <info@kalwaltart.it>
2021-11-20 13:59:19 +0100

    setup with a fixed obfuscated dimension

--- example/js/main.js
+++ example/js/main.js
@@ -1,11 +1,11 @@
 window.addEventListener('FeatureSETDisplay-loaded', function () {
-  var arfset = new ARfset(893, 1117);
+  var arfset = new ARfset();
   arfset.loadNFTMarker('data/pinball', (nft) => { 
     console.log(nft);
-    console.log(nft.nftPoints.size()) //this give zero
+    //console.log(nft.nftPoints.size()) //this give zero
     const nftPointsPointer = nft.nftPoints.$$.ptr;
     const nftArray = new Int16Array(Module.HEAP16.buffer, nftPointsPointer, nft.numFpoints * 2);
-    console.log(nftArray);
+    //console.log(nftArray);
   });
   document.addEventListener('nftMarker', (ev) => {
     //console.log( ev.detail.nftPoints.$$.ptr );
