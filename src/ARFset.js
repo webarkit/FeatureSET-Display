8ce89dae99dac1750f69e4ac3113ae0a7dfd9eab
kalwalt <github@kalwaltart.it>
2022-03-22 19:15:36 +0100

    new version 0.3.0

--- src/ARFset.js
+++ src/ARFset.js
@@ -52,7 +52,7 @@ export default class ARFset {
         this.numFpoints = null;
         this.canvas = null;
         this.ctx = null;
-        this.version = '0.2.2';
+        this.version = '0.3.0';
         console.log('FeatureSETDisplay version: ', this.version);
     }
 
